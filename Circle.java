bae30b6427ad1b0b2a6bbfeb7c7d4c9cffdbd326
maniibon <54945278+maniibon@users.noreply.github.com>
2019-09-25 10:06:34 -0400

    Add files via upload

--- Circle.java
+++ Circle.java
@@ -1,13 +1,24 @@
 package Homework.Homework2;
 
-class Circle
-{
-public static void main(String[]args)
-        {
-        int radius = 8;
-        double area = Math.PI * (radius*radius);
-        System.out.println("The area of circle is : " + area);
-        double circumference= Math.PI*radius;
-        System. out.println("The circumference of the circle is:"+ circumference);
-        }
+class Circle{
+       int radius;
+       int  area;
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+    public double getArea(){
+        return Math.PI*(radius*radius);
+    }
+    public static void main(String[]args){
+Circle circle = new Circle();
+         circle.setRadius(8);
+    circle.getArea();
+    System.out.print(circle.getArea());
+    }
+
 }
