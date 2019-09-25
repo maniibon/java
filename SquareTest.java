4c3b0702a80eac600c854946edc6e627b8c1ae56
maniibon <54945278+maniibon@users.noreply.github.com>
2019-09-25 10:25:45 -0400

    Add files via upload

---
+++ SquareTest.java
@@ -0,0 +1,11 @@
+package Homework.Homework2;
+
+import Homework.Homework1.Squarepart2;
+
+public class SquareTest {
+    public static void main(String[] args) {
+        Squarepart2 square = new Squarepart2(2);
+        square.calculateArea();
+        System.out.println(square);
+    }
+}
